6cc9511d67fd8d3aa136953b0d9c05d6676f105c
AKASH KAMANE <akashkamane97@gmail.com>
2019-08-16 22:55:14 +0530

    aas

---
+++ SpringWithXML/src/main/java/com/example/demo/AOP/Participants.java
@@ -0,0 +1,42 @@
+package com.example.demo.AOP;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class Participants {
+
+	 
+	String technology;
+	
+	
+	public void takeSeat() {
+		System.out.println("Taking seat");
+	}
+	public void switchOffPhones() {
+		System.out.println("Switching Off There phones");
+	}
+	public void applaud() {
+		System.out.println("Clap Clap Clap Clap");
+	}
+	public void claimRefund() {
+		System.out.println("Boo Want claim Refund");
+	}
+	public void undergoTraining(ProceedingJoinPoint joinPoint) {
+		{
+			try {
+				System.out.println("Taking seat");
+				System.out.println("Switching Off There phones");
+				long start=System.currentTimeMillis();
+				joinPoint.proceed();
+				long end=System.currentTimeMillis();
+				System.out.println("Clap Clap Clap Clap");
+				System.out.println("The Training took "+(end - start)+" milliseconds");
+			} catch (Throwable e) {
+				System.out.println("Boo Want claim Refund");
+			}
+
+		}
+	}
+	public void predictTraining(String technology) {
+		System.out.println("Participants are predicting that training will train on "+technology );
+	}
+}
---
+++ SpringWithXML/src/main/java/com/example/demo/SpringWithXmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithXmlApplication.class, args);
+	}
+
+}
